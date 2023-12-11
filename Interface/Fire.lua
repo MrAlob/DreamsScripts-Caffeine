e349c4378109876e0b1a5e10e86cc454d80d31b9
Dreamzlol <denis.dasilvarocha@icloud.com>
2023-12-11 20:21:58 +0100

    1.0.4 version number

--- Interface/Fire.lua
+++ Interface/Fire.lua
@@ -22,7 +22,7 @@ Rotation.Config:Write("decurse", false)
 Rotation.Config:Write("spellsteal", false)
 
 Caffeine:Print("Dreams|cff00B5FFScripts |cffFFFFFF - Hello! Rotation successfully initialized.")
-Caffeine:Print("Dreams|cff00B5FFScripts |cffFFFFFF - Version: 1.0.3")
+Caffeine:Print("Dreams|cff00B5FFScripts |cffFFFFFF - Version: 1.0.4")
 Caffeine:Print("Dreams|cff00B5FFScripts |cffFFFFFF - If you need any help or have suggestions.")
 Caffeine:Print("Dreams|cff00B5FFScripts |cffFFFFFF - Discord: |cffeb6ee9https://discord.gg/dJ2upysMcW")
 
