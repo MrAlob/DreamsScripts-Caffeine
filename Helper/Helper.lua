c292ad98a766d22cd8e7c232001291f6808b8412
Dreamzlol <denis.dasilvarocha@icloud.com>
2024-01-08 18:26:45 +0100

    Add event handling and drawing functionality

--- Helper/Helper.lua
+++ Helper/Helper.lua
@@ -2,6 +2,7 @@ local Unlocker, Caffeine, Rotation = ...
 
 -- Units
 local Player = Caffeine.UnitManager:Get("player")
+local Target = Caffeine.UnitManager:Get("target")
 
 -- Spells
 local spells = Rotation.Spells
@@ -38,7 +39,7 @@ end
 function Caffeine.Unit:CustomIsBoss()
 	local id = ObjectID(self:GetOMToken())
 
-	-- Lady Deathwhisper (36855)
+	-- Lady Deathwhisper
 	if id == 36855 then
 		return true
 	end
@@ -141,3 +142,151 @@ end
 function Caffeine.Unit:UsingVehicle()
 	return UnitUsingVehicle(self:GetOMToken())
 end
+
+-- Event Spell IDs
+local spellTable = {
+	[72762] = true, -- Lich King: Defile
+	[72040] = true, -- Blood Council: Conjure Empowered Flame
+	[70126] = true, -- Sindragossa: Beacon
+}
+
+local Eventmanager = Caffeine.EventManager:New()
+Eventmanager:RegisterWoWEvent("COMBAT_LOG_EVENT_UNFILTERED", function()
+	local _, subEvent, _, sourceGUID, _, _, _, destGUID, _, _, _, spellID = CombatLogGetCurrentEventInfo()
+
+	if not spellTable[spellID] then
+		return
+	end
+
+    if subEvent == "SPELL_CAST_START" and spellID == 72762 then
+		local currentTargetGUID = UnitGUID("target")
+        if currentTargetGUID == Player:GetGUID() then
+            print("|cffffffffDreams|cff00B5FFScripts|cffffffff: Defile on you! Using Nitro Boosts")
+			SpellStopCasting()
+			UseInventoryItem(8)
+		end
+    elseif subEvent == "SPELL_CAST_SUCCESS" and spellID == 72040 then
+		if destGUID == Player:GetGUID() then
+            print("|cffffffffDreams|cff00B5FFScripts|cffffffff: Orb on you! Using Nitro Boosts")
+			SpellStopCasting()
+			UseInventoryItem(8)
+		end
+    elseif subEvent == "SPELL_AURA_APPLIED" and spellID == 70126 then
+		if destGUID == Player:GetGUID() then
+            print("|cffffffffDreams|cff00B5FFScripts|cffffffff: Beacon on you! Using Nitro Boosts")
+			SpellStopCasting()
+			UseInventoryItem(8)
+		end
+	end
+end)
+
+-- Enemies
+local drawEnemiesIDs = {
+	[38222] = true, -- Vengeful Shade
+	[38711] = true, -- Bone Spike
+	[36619] = true, -- Bone Spike
+	[38712] = true, -- Bone Spike
+}
+
+local function GetDrawingEnemies()
+	local drawEnemies = {}
+
+	Caffeine.UnitManager:EnumEnemies(function(unit)
+		if unit and unit:Exists() and not unit:IsDead() and Player:GetDistance(unit) <= 40 and drawEnemiesIDs[unit:GetID()] then
+			table.insert(drawEnemies, unit)
+		end
+	end)
+
+	return drawEnemies
+end
+
+-- Friends
+local debuffs = Caffeine.Globals.SpellBook:GetList(71289, 69279, 69240, 71218, 73019, 73020, 70911, 70126)
+local function GetDrawingFriends()
+	local drawFriends = {}
+
+	Caffeine.UnitManager:EnumFriends(function(unit)
+		if not unit then
+			return
+		end
+		if not unit:Exists() then
+			return
+		end
+		if unit:IsDead() then
+			return
+		end
+		if Player:GetDistance(unit) > 40 then
+			return
+		end
+
+		if unit:GetAuras():FindAnyFrom(debuffs):IsUp() then
+			table.insert(drawFriends, unit)
+		end
+	end)
+
+	return drawFriends
+end
+
+-- Draw
+local Draw = Caffeine.Draw:New()
+Draw:Sync(function(draw)
+	local pPos = Player:GetPosition()
+	local drawEnemies = GetDrawingEnemies()
+	local drawFriends = GetDrawingFriends()
+
+	-- Enemies
+	if drawEnemies then
+		for i, object in ipairs(drawEnemies) do
+			if not object:Exists() then
+				return
+			end
+			if not object:GetPosition() then
+				return
+			end
+			local ePos = object:GetPosition()
+
+			draw:SetColor(255, 0, 0, 100)
+			draw:SetWidth(2)
+			draw:Line(pPos.x, pPos.y, pPos.z, ePos.x, ePos.y, ePos.z, 40)
+
+			draw:SetColor(255, 255, 255, 100)
+			draw:Outline(ePos.x, ePos.y, ePos.z, 5)
+
+			draw:SetColor(255, 0, 0, 100)
+			draw:FilledCircle(ePos.x, ePos.y, ePos.z, 5)
+
+			draw:SetColor(255, 255, 255, 200)
+			draw:Text(object:GetName(), "GameFontNormal", ePos.x, ePos.y, ePos.z)
+		end
+	end
+
+	-- Friends
+	if drawFriends then
+		for i, object in ipairs(drawFriends) do
+			if not object:Exists() then
+				return
+			end
+			if not object:GetPosition() then
+				return
+			end
+			local ePos = object:GetPosition()
+
+			draw:SetColor(255, 165, 0, 100)
+			draw:SetWidth(2)
+			draw:Line(pPos.x, pPos.y, pPos.z, ePos.x, ePos.y, ePos.z, 40)
+
+			draw:SetColor(255, 255, 255, 100)
+			draw:Outline(ePos.x, ePos.y, ePos.z, 5)
+
+			draw:SetColor(255, 165, 0, 100)
+			draw:FilledCircle(ePos.x, ePos.y, ePos.z, 5)
+
+			draw:SetColor(255, 255, 255, 200)
+			draw:Text(object:GetName(), "GameFontNormal", ePos.x, ePos.y, ePos.z)
+		end
+	end
+
+	return false
+end)
+
+Draw:Enable()
